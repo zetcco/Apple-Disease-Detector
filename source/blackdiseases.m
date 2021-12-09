c134c40b357d475dcd3935bc0cf1f3cab9d40541
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2021-12-09 06:07:00 +0530

    Added black disease identification

--- source/blackdiseases.m
+++ source/blackdiseases.m
@@ -1,4 +1,4 @@
-diseases = {'ac-s-','fs-s-'};
+diseases = {'fs-s-','ac-s-'};
 pixels = zeros(1,length(diseases));
 disease_name = {};
 last = 1;
@@ -9,16 +9,16 @@ for diseaseno = 1:length(diseases)
         img_bw = rgb2gray(img_original);
 
         % Gray level slicing
-        k=double(img_bw);
-        [row,col]=size(k);
-        T1=0;
-        T2=100;
+        bin_black_disease=double(img_bw);
+        [row,col]=size(bin_black_disease);
+        black_disease_l1=0;
+        black_disease_l2=100;
         for x=1:row            
             for y=1:col        
-                if((img_bw(x,y)>T1) && (img_bw(x,y)<T2))
-                    k(x,y)=255;
+                if((img_bw(x,y)>black_disease_l1) && (img_bw(x,y)<black_disease_l2))
+                    bin_black_disease(x,y)=255;
                 else
-                    k(x,y)=0;
+                    bin_black_disease(x,y)=0;
                 end
             end
         end
@@ -27,18 +27,19 @@ for diseaseno = 1:length(diseases)
         [edges, thresh] = edge(img_bw,'Roberts');
         sens = thresh + 0.07;
         imgsep = edge(img_bw,'Roberts', sens);
+        figure(c+2), imshow(imgsep);
 
         % Get 4-m connected neighbors
-        [L, n] = bwlabel(k&imgsep, 4);
+        [L, n] = bwlabel(bin_black_disease&imgsep, 4);
 
         % Append number of 4-m conncted regions
         pixels(last) = n;
         
         figure(c), 
             subplot(2,2,1), imshow(img_bw), title(img_filename),
-            subplot(2,2,2), imshow(k), 
+            subplot(2,2,2), imshow(bin_black_disease), 
             subplot(2,2,3), imshow(imgsep), 
-            subplot(2,2,4), imshow(k&imgsep), title(strcat('4-m Pixels = ', int2str(n)));
+            subplot(2,2,4), imshow(bin_black_disease&imgsep), title(strcat('4-m Pixels = ', int2str(n)));
 
         input('Press enter..');
         
